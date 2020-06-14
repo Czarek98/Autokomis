37260e4f639b1534acb79400fa58526e01a21738
Czarek <cezary.bohdanowicz@gmail.com>
2020-06-14 13:52:34 +0200

    Menu planning

--- src/com/company/CezaryBohdanowicz/Car.java
+++ src/com/company/CezaryBohdanowicz/Car.java
@@ -2,12 +2,22 @@ package com.company.CezaryBohdanowicz;
 
 public class Car {
 
-    public Car(String brand, String model, Integer mileage, String color, String segment) {
+    public final String brand;
+    public final String model;
+    public final Integer mileage;
+    public final String color;
+    public final Double price;
+    public String segment;
+    static String PREMIUM;
+    static String STANDARD;
+    static String BUDGET;
+
+    public Car(String brand, String model, Integer mileage, String color, String segment, Double price) {
         this.brand = brand;
         this.model = model;
         this.mileage = mileage;
         this.color = color;
-       // this.segment = segment;
+        this.price = price;
 
         if (segment == "premium"){
             this.segment = PREMIUM;
@@ -17,16 +27,5 @@ public class Car {
             this.segment=BUDGET;
         }
     }
-
-    public final String brand;
-   public final String model;
-   public final Integer mileage;
-   public final String color;
-   public String segment;
-   static String PREMIUM;
-   static String STANDARD;
-   static String BUDGET;
-
-
 
 }
--- src/com/company/CezaryBohdanowicz/Main.java
+++ src/com/company/CezaryBohdanowicz/Main.java
@@ -1,8 +1,95 @@
 package com.company.CezaryBohdanowicz;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Main {
+    static boolean exit;
 
     public static void main(String[] args) {
 
+
+        Menu menu = new Menu();
+        printHeader();
+        while (!exit) {
+            printMenu();
+            int choice = getMenuChoice();
+            performAction(choice);
+        }
+
     }
-}
+
+        private static void printHeader() {
+            System.out.println("+-----------------------------------+");
+            System.out.println("|          Car Dealership           |");
+            System.out.println("|              GAME                 |");
+            System.out.println("+-----------------------------------+");
+        }
+
+        private static void printMenu() {
+            displayHeader("Please make a selection");
+            System.out.println("1) List of car to buy");
+            System.out.println("2) Your list of car");
+            System.out.println("3) Your account balance");
+            System.out.println("4) Repair your car");
+            System.out.println("5) Number of transactions");
+            System.out.println("6) Price list");
+            System.out.println("0) Exit");
+        }
+
+        private static int getMenuChoice() {
+            Scanner keyboard = new Scanner(System.in);
+            int choice = -1;
+            do {
+                System.out.print("Enter your choice: ");
+                try {
+                    choice = Integer.parseInt(keyboard.nextLine());
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid selection. Numbers only please.");
+                }
+                if (choice < 0 || choice > 4) {
+                    System.out.println("Choice outside of range. Please chose again.");
+                }
+            } while (choice < 0 || choice > 4);
+            return choice;
+        }
+
+        private static void performAction(int choice) {
+            switch (choice) {
+                case 0:
+                    System.out.println("Thank you for using our application.");
+                    System.exit(0);
+                    break;
+                case 1:
+                    //function
+                    break;
+                case 2:
+                    //function
+                    break;
+                case 3:
+                    //function
+                    break;
+                case 4:
+                    //function
+                    break;
+                default:
+                    System.out.println("Unknown error has occured.");
+            }
+        }
+
+        private static void displayHeader(String message){
+            System.out.println();
+            int width = message.length() + 6;
+            StringBuilder sb = new StringBuilder();
+            sb.append("+");
+            for(int i = 0; i < width; ++i){
+                sb.append("-");
+            }
+            sb.append("+");
+            System.out.println(sb.toString());
+            System.out.println("|   " + message + "   |");
+            System.out.println(sb.toString());
+        }
+
+    }
+
